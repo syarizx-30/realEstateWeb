a1553e9e91c45174ab9508dc85267a1a1ae9b79b
syarizx-30 <rizalsyamsu892@gmail.com>
2025-06-27 18:26:42 +0700

    Get Started

---
+++ realestate-web-app/src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <div>
+      <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+          <div className="flexColCenter inner-container">
+            <span className="primaryText">Get Started with Homyz</span>
+            <span className="secondaryText">
+              Subscribe and find super attractive price quotes
+              <br />
+              Find your residence soon
+            </span>
+            <button className="button">
+                <a href="https://www.instagram.com/syarizx_/">Get Started</a>
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default GetStarted;
