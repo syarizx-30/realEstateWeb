ebf51e6c1bc56203ba7d0b4b486920558228789d
syarizx-30 <rizalsyamsu892@gmail.com>
2025-06-29 18:49:52 +0700

    making Responsive

--- realestate-web-app/src/components/Header/Header.jsx
+++ realestate-web-app/src/components/Header/Header.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 import './Header.css'
+import { BiMenuAltRight } from 'react-icons/bi'
+import OutsideClickHandler from "react-outside-click-handler";
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const getMenuStyles = (menuOpened) => {
+    if (document.body.clientWidth <= 800) {
+      return {right: !menuOpened && "-100%"}
+    }
+  }
   return (
     <div>
       <section className="h-wrapper">
         <div className="flexCenter paddings innerWidth h-container">
             <img src="./logo.png" alt="logo" width={100} />
 
-            <div className="flexCenter h-menu">
+            <OutsideClickHandler
+            onOutsideClick={( ) => {
+
+              setMenuOpened(false) 
+            }}
+            >
+            <div className="flexCenter h-menu"
+            style={getMenuStyles(menuOpened)}
+            >
                 <a href="">Residencies</a>
                 <a href="">Our Value</a>
                 <a href="">Contact Us</a>
@@ -16,6 +33,11 @@ const Header = () => {
                 <a href="">Contact</a>
                 </button>
             </div>
+            </OutsideClickHandler>
+        <div className="menu-icon"
+        onClick={() => setMenuOpened((prev) => !prev)}>
+          <BiMenuAltRight size={30} />
+        </div>
         </div>
       </section>
     </div>
